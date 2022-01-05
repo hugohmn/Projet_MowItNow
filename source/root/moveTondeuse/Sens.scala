7dcc201cfcc68d56280133b0f27d40c4d3948a24
Hugo Hamon <78233733+hugohmn@users.noreply.github.com>
2022-01-05 20:46:54 +0100

    Delete source directory

--- source/root/moveTondeuse/Sens.scala
+++
@@ -1,9 +0,0 @@
-package moveTondeuse
-
-/*Classe qui prend en compte la sens de la tondeuse*/
-
-object Sens extends Enumeration {
-  //Enumeration permet de rentrer plusieurs valeurs possibles (ici 4)
-  type Sens = Value
-  val N, E, S, O = Value
-}
