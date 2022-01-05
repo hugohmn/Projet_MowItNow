fb36578195e0b7dae72953e55c68c965c0288824
Hugo Hamon <78233733+hugohmn@users.noreply.github.com>
2022-01-05 17:34:17 +0100

    Add files via upload

---
+++ MowItNow/source/root/moveTondeuse/Position.scala
@@ -0,0 +1,11 @@
+package moveTondeuse
+
+class Position (val _y : Int, val _x:Int, val _sens : Sens.Value) {
+  var x: Int = _x
+  var y: Int = _y
+  var sens: Sens.Value = _sens
+  override def toString = s"$x $y ${sens.toString.charAt(0)}"
+}
+
+
+
