dec2066f9a8b85c9b2025f6fff6c262ba7aba301
Hugo Hamon <78233733+hugohmn@users.noreply.github.com>
2022-01-05 20:04:17 +0100

    Delete source directory

--- source/root/all.scala
+++
@@ -1,7 +0,0 @@
-import moveTondeuse.Surface
-
-/*Script prenant en entrée le ficheir d'input et qui fournit ensuite les outputs en sortie*/
-
-object all extends App{
-  Surface.run("Input.txt")
-}
--- source/root/moveTondeuse/Sens.scala
+++
@@ -1,7 +0,0 @@
-package moveTondeuse
-
-/*Classe qui prend en compte la sens de la tondeuse*/
-
-object Sens extends Enumeration {
-  val NORD, EST, SUD, OUEST = Value
-}
