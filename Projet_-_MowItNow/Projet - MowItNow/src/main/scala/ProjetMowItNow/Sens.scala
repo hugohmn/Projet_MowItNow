09925c725b8a44e4ee26602f4ed1e18f5441c1d8
Hugo Hamon <78233733+hugohmn@users.noreply.github.com>
2022-01-05 17:33:56 +0100

    Delete Projet_-_MowItNow directory

--- Projet_-_MowItNow/Projet - MowItNow/src/main/scala/ProjetMowItNow/Sens.scala
+++
@@ -1,8 +0,0 @@
-package ProjetMowItNow
-
-/*Nous choisissons d'utiliser le type énumération car il permet de spécifier l'ensemble des valeurs prises
-par une variable/constante (ici constante).*/
-
-object Sens extends Enumeration {
-  val NORD, EST, SUD, OUEST = Value
-}
--- Projet_-_MowItNow/Projet - MowItNow/src/main/scala/Run.scala
+++
@@ -1,6 +0,0 @@
-import ProjetMowItNow.Surface
-
-/*A partir du fichier d'input (Input.txt), la tondeuse se déplace*/
-object Run extends App{
-  Surface.run("Input.txt")
-}
