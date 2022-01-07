a47092bb0545306dda0b0471a23764bd0519b54b
Hugo Hamon <78233733+hugohmn@users.noreply.github.com>
2022-01-07 10:17:45 +0100

    Delete source directory

--- source/root/moveTondeuse/Direction.scala
+++
@@ -1,9 +0,0 @@
-package moveTondeuse
-
-/*Classe qui prend en compte la sens de la tondeuse*/
-
-object Direction extends Enumeration {
-  //Enumeration permet de rentrer plusieurs valeurs possibles (ici 4)
-  type Direction = Value
-  val N, E, S, O = Value
-}
